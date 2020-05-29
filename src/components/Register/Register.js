6f637dfc9a2a58868a586a91258147c742a897dc
pechaprasanna <rpv31dec1991@gmail.com>
2020-05-29 09:31:47 -0700

    deploy

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 
 	onSubmitSignIn = (event) => {
 		event.preventDefault();
-		fetch('http://localhost:3000/register',{
+		fetch('https://arcane-reaches-19460.herokuapp.com/register',{
 			method: 'post',
 			headers: {'Content-Type':'application/json'},
 			body: JSON.stringify({
